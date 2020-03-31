91cab17ee33ae3392721b89f0cdc1154b984dcd6
lesia-s <lesiiasergienko@gmail.com>
2020-03-31 23:55:35 +0300

    edit

--- .bood-install.sh
+++ .bood-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/lesia-s/design-practice-2/blob/master/examples/binary/out/bin/bood_rebase
+wget https://github.com/lesia-s/design-practice-2/examples/binary/out/bin/bood_rebase
 sudo chmod +x bood_rebase
 mv bood_rebase $GOPATH/bin
